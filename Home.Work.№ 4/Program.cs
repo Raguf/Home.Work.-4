9b8b82c027cd2c8b1b6be8f459a8f0aa6d70cb58
Raguf <ragufym@code.edu.az>
2022-01-21 02:06:36 +0400

    Add project files.

---
+++ Home.Work.№ 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Home.Work.__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1-50 arasi (1 ve 50 de daxil olmaqla [1,50]) butun tek ededleri ile alqoritm yazmaq*/
+
+            for (int i = 0; i < 50; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+
+            Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
